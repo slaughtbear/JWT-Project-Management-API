a76dfedd651ed0c494105f1b590101de3b08da9c
slaughtbear <ivanaguirre368@gmail.com>
2025-02-17 10:47:19 -0600

    include tasks router

--- src/main.py
+++ src/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from src.routes.tasks import tasks_router
 
 app = FastAPI()
 
 @app.get("/")
 async def main():
     return {"message": "welcome to my to-do list api"}
+
+app.include_router(
+    tasks_router,
+    prefix = "/tasks",
+    tags = ["Tasks"]
+)
