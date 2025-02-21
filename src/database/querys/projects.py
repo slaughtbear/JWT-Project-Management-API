9919ae536bf89ca556bec3dc30b90d2f3f78267e
slaughtbear <ivanaguirre368@gmail.com>
2025-02-21 17:51:49 -0600

    add projects db querys

--- src/database/querys/projects.py
+++ src/database/querys/projects.py
@@ -1,5 +1,11 @@
 from ..config import supabase
 
+
+def get_all_projects_bd():
+    response = supabase.table("projects").select("*").execute()
+    return response.data
+
+
 def get_one_project(id: int):
     response = supabase.table("projects").select("id").eq("id", id).execute()
-    return response.data
+    return response.data[0]
