d1814f6b522f20e258b4c0ef6b43a8d43dd6157e
slaughtbear <ivanaguirre368@gmail.com>
2025-02-21 22:58:40 -0600

    add: projects and jwt_auth routes

--- src/main.py
+++ src/main.py
@@ -1,14 +1,33 @@
 from fastapi import FastAPI
 from src.routes.tasks import tasks_router
+from src.routes.projects import projects_router
+from src.routes.jwt_auth import jwt_auth
+
 
 app = FastAPI()
+
 
 @app.get("/")
 async def main():
     return {"message": "welcome to my to-do list api"}
 
+
 app.include_router(
     tasks_router,
     prefix = "/tasks",
     tags = ["Tasks"]
+)
+
+
+app.include_router(
+    projects_router,
+    prefix = "/projects",
+    tags = ["Projects"]
+)
+
+
+app.include_router(
+    jwt_auth,
+    prefix = "/auth",
+    tags = ["Authentication"]
 )
