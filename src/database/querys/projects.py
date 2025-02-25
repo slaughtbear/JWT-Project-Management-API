4402f0fdf4bf0b0e5bd5f8198dd35a538731a29a
slaughtbear <ivanaguirre368@gmail.com>
2025-02-25 12:34:05 -0600

    add all crud querys

--- src/database/querys/projects.py
+++ src/database/querys/projects.py
@@ -6,6 +6,26 @@ def get_all_projects_bd():
     return response.data
 
 
-def get_one_project(id: int):
-    response = supabase.table("projects").select("id").eq("id", id).execute()
+def get_one_project_bd(id: int):
+    response = supabase.table("projects").select("*").eq("id", id).execute()
+    return None if not response.data else response.data[0]
+
+
+def get_one_project_by_name_bd(name: str):
+    response = supabase.table("projects").select("*").eq("name", name).execute()
+    return None if not response.data else response.data[0]
+
+
+def create_project_bd(project_data):
+    response = supabase.table("projects").insert(project_data.model_dump()).execute()
     return response.data[0]
+
+    
+def update_project_bd(project_data, id: int):
+    response = supabase.table("projects").update(project_data.model_dump()).eq("id", id).execute()
+    return response.data[0]
+
+
+def delete_project_bd(id: int):
+    response = supabase.table("projects").delete().eq("id", id).execute()
+    return response.data
