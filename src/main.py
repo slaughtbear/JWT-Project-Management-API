c74f1e9998f614a1d017367c9910c6e144a50e63
slaughtbear <ivanaguirre368@gmail.com>
2025-02-25 12:41:48 -0600

    add /users router

--- src/main.py
+++ src/main.py
@@ -2,9 +2,17 @@ from fastapi import FastAPI
 from src.routes.tasks import tasks_router
 from src.routes.projects import projects_router
 from src.routes.jwt_auth import jwt_auth
+from src.routes.users import users_router
 
 
 app = FastAPI()
+
+
+app.include_router(
+    users_router,
+    prefix = "/users",
+    tags = ["Users"]
+)
 
 
 app.include_router(
