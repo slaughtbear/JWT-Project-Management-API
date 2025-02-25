ed82d4bfa3d1614f7172e4f18b7a103c0e23a5c5
slaughtbear <ivanaguirre368@gmail.com>
2025-02-25 12:45:15 -0600

    add project schemas

--- src/schemas/project.py
+++ src/schemas/project.py
@@ -1,6 +1,22 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import Optional
+
 
 class Project(BaseModel):
+    """Esquema base para un proyecto."""
     id: int
     created_at: str
     name: str
+    description: Optional[str] = None
+
+
+class ProjectCreate(BaseModel):
+    """Esquema para la creación de un proyecto."""
+    name: str = Field(max_length=100, min_length=3)
+    description: Optional[str] = Field(None, max_length=255)
+
+
+class ProjectUpdate(BaseModel):
+    """Esquema para actualizar datos del proyecto."""
+    name: Optional[str] = Field(None, max_length=100, min_length=3)
+    description: Optional[str] = Field(None, max_length=255)
