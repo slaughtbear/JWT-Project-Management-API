bc47ba6dda3700520ca236d39b967570c1e0ce44
slaughtbear <ivanaguirre368@gmail.com>
2025-03-05 17:33:07 -0600

    update querys params

--- src/database/querys/projects.py
+++ src/database/querys/projects.py
@@ -1,13 +1,13 @@
 from ..config import supabase
 
 
-def get_all_projects_bd():
-    response = supabase.table("projects").select("*").execute()
+def get_all_projects_bd(id_user: int):
+    response = supabase.table("projects").select("*").eq("id_user", id_user).execute()
     return response.data
 
 
-def get_one_project_bd(id: int):
-    response = supabase.table("projects").select("*").eq("id", id).execute()
+def get_one_project_bd(id_project: int, id_user: int):
+    response = supabase.table("projects").select("*").eq("id", id_project).eq("id_user", id_user).execute()
     return None if not response.data else response.data[0]
 
 
@@ -16,8 +16,17 @@ def get_one_project_by_name_bd(name: str):
     return None if not response.data else response.data[0]
 
 
-def create_project_bd(project_data):
-    response = supabase.table("projects").insert(project_data.model_dump()).execute()
+def create_project_bd(project_data, current_user):
+    project_dict = project_data.model_dump()
+    response = (
+        supabase.table("projects")
+        .insert({
+            "name": project_dict["name"],
+            "description": project_dict["description"],
+            "id_user": current_user["id"]
+        })
+        .execute()
+    )
     return response.data[0]
 
     
